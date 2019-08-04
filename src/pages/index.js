328d69a06e56a2c7f21262581dafd6ee837d6832
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-08-04 13:20:00 -0700

    base gatsby structure

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `pink` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg"/>
+    
+  </div>
+)
