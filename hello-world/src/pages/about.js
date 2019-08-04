88259d788f7792293f5921f4bb8a95fc17410fb2
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-08-04 16:28:12 -0700

    imported header text

--- hello-world/src/pages/about.js
+++ hello-world/src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import Header from "../components/header"
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header />
+    <Header headerText="about gatsby"/>
     <p>Such wow. Very React.</p>
   </div>
 )
