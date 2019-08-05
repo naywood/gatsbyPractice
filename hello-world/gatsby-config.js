c249933a601edb583ac8e42afbe403328a615335
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-08-05 13:22:49 -0700

    imported css

--- hello-world/gatsby-config.js
+++ hello-world/gatsby-config.js
@@ -3,7 +3,6 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-import "./src/styles/global.css"
 
 module.exports = {
   /* Your site config here */
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+
 import Header from "../components/header"
 export default () => (
   <div style={{ color: `purple` }}>
